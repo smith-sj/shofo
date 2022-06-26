3328f4a5037ce3d84d7907099e96792d117107f1
smith-sj <mr.sjsmith@protonmail.com>
2022-06-26 12:23:14 +1000

    Added necesary pages to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def mission
+  end
+
+  def browse
+  end
+
+  def help
+  end
+  
+  def host
+  end
+
+  def wallet
+  end
+
+  def calendar
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'about', to:'pages#about', as: 'about'
+  get 'mission', to:'pages#mission', as: 'mission'
+  get 'browse', to:'pages#browse', as: 'browse'
+  get 'help', to: 'pages#help', as: 'help'
+  get 'host', to: 'pages#host', as: 'host'
+  get 'wallet', to: 'pages#wallet', as: 'wallet'
+  get 'calendar', to: 'pages#calendar', as: 'calendar'
+
+
   resources :addresses
   devise_for :users
   root 'pages#home'
