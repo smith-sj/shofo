2dbf4062f9096fd57cb02382f254711536c3a5ba
smith-sj <mr.sjsmith@protonmail.com>
2022-06-27 18:35:04 +1000

    edited migratoins to fix heroku error

--- db/migrate/20220627025046_create_satuses.rb
+++
@@ -1,9 +0,0 @@
-class CreateSatuses < ActiveRecord::Migration[6.1]
-  def change
-    create_table :satuses do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220627045554_drop_statuses.rb
+++
@@ -1,5 +0,0 @@
-class DropStatuses < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :statuses
-  end
-end
