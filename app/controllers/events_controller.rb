dc6d11ab6cc1dc817c95925bc70106ca609b7d42
smith-sj <mr.sjsmith@protonmail.com>
2022-06-27 19:31:21 +1000

    implemented authroization

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
   before_action :set_form_vars
+  before_action :authenticate_user!, except: %i[ index show ]
 
   # GET /events or /events.json
   def index
