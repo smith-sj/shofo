9f9b74c6ec41a165d75ede07ba9ff75eb0482d06
smith-sj <mr.sjsmith@protonmail.com>
2022-06-27 12:49:16 +1000

    added categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_094753) do
+ActiveRecord::Schema.define(version: 2022_06_27_024133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 2022_06_23_094753) do
     t.string "suburb", null: false
     t.string "state", null: false
     t.string "postcode", limit: 4, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
