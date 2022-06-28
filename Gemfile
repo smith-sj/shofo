9e22a10c311054e9f491a685b7f4344233d3c280
smith-sj <mr.sjsmith@protonmail.com>
2022-06-28 17:26:09 +1000

    added after_find callback to event model that updates its status based on the current time and the event end date. Updated the host view with different sections for different statuses.

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.8"
 gem  'addressfinder'
+gem 'whenever'
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,16 @@ class Event < ApplicationRecord
     completed: 3,
     cancelled: 4
   }
+
+  after_find do |event|
+    return if event.event_status == 4
+    if DateTime.now < event.start_date
+      update_attribute(:event_status, 1)
+    elsif DateTime.now >= event.start_date && DateTime.now < event.end_date 
+      update_attribute(:event_status, 2)
+    elsif DateTime.now >= event.end_date 
+      update_attribute(:event_status, 3)
+    end
+  end
+
 end
