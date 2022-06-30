4019d6a4abd5b8b721baf9b2bedf0e580017ab82
smith-sj <mr.sjsmith@protonmail.com>
2022-07-01 00:18:02 +1000

    added line in config for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Shofo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.initialize_on_precompile = false
 
     # Don't generate system test files.
     config.generators.system_tests = nil
