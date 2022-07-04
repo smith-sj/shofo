47f7c9c126db0c3b8d377e7083eed5227a81d938
smith-sj <mr.sjsmith@protonmail.com>
2022-07-05 00:44:26 +1000

    trying to fix heorku bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -131,6 +131,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :start_date, :end_date, :address, :venue, :private, :capacity, :price, :user_id, :event_status, :category_id, :sub_category, :event_image)
+      params.require(:event).permit(:title, :description, :start_date, :end_date, :address, :venue, :private, :capacity, :price, :user_id, :event_status, :category_id, :sub_category, :event_image, :longitude, :latitude)
     end
 end
