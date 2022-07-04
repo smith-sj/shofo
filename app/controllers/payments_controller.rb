cfe6577ceedb8a0171899f8378f210949a8d2d7d
smith-sj <mr.sjsmith@protonmail.com>
2022-07-05 09:03:38 +1000

    fixed ticket booking bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,25 +1,16 @@
 class PaymentsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:webhook]
+    skip_before_action :verify_authenticity_token, only: %i[webhook success]
 
     def success
+        @event_id = params[:eventId]
     end
 
     def webhook
         payment_id = params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retrieve(payment_id)
-        event_id = payment.metadata.event_id
-        user_id = payment.metadata.user_id
-        seller_id = payment.metadata.seller_id
-        p "Event id: #{event_id}"
-        p "User id: #{user_id}"
-        p "Seller id: #{seller_id}"
-        p "Success"
-
-        Ticket.create(
-            event_id: event_id,
-            seller_id: seller_id,
-            holder_id: user_id
-          )
+        @event_id = payment.metadata.event_id
+        @user_id = payment.metadata.user_id
+        @seller_id = payment.metadata.seller_id
     end
 
 end
