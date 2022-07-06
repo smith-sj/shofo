37621126e5f2f9fc3c29924a2b402f6db981bb36
smith-sj <mr.sjsmith@protonmail.com>
2022-07-06 17:57:16 +1000

    fixed another vlaidation error

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,25 +4,28 @@ class TicketsController < ApplicationController
     helper_method :qr_code
     before_action :authenticate_user!
 
-
-    
     def validate
         @ticket_holder = nil
+        @ticket_holder_name = nil
         @ticket_seller = nil
+        @seller_holder_name = nil
         @ticket_event = nil
         @is_minor = nil
         if @ticket == nil
             @validation_message = "This is not a valid ticket."
         elsif @ticket.seller_id != current_user.id
             @ticket_holder = User.find(@ticket.holder_id)
-            @validation_message = "Ticket ##{@ticket.id}) belonging to #{@ticket_holer.first_name} #{@ticket_holer.last_name}. 
+            @ticket_holer_name = "#{@ticket_holder.first_name} #{@ticket_holder.last_name}"
+            @validation_message = "Ticket ##{@ticket.id}) belonging to #{@ticket_holder_name}. 
             You must be logged in as the Host or Ticket Holder to view more information."
         else
             @ticket_holder = User.find(@ticket.holder_id)
+            @ticket_holer_name = "#{@ticket_holder.first_name} #{@ticket_holder.last_name}"
             @ticket_seller = User.find(@ticket.seller_id)
+            @ticket_seller_name = "#{@ticket_seller.first_name} #{@ticket_seller.last_name}"
             @ticket_event = Event.find(@ticket.event_id)
             @is_minor = 18 > ((Time.zone.now - @ticket_holder.date_of_birth.to_time) / 1.year.seconds).floor
-            @validation_method = "Ticket ##{@ticket.id}) belonging to #{@ticket_holer.first_name} #{@ticket_holer.last_name}."
+            @validation_method = "Ticket ##{@ticket.id}) belonging to #{@ticket_holder_name}."
         end
     end
 
