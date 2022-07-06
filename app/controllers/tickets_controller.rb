8bbccee489707b1b358365b4c673bca520a6d103
smith-sj <mr.sjsmith@protonmail.com>
2022-07-06 16:42:50 +1000

    fixed validation authorization

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,15 +3,27 @@ class TicketsController < ApplicationController
     before_action :get_tickets, only: %i[ wallet ]
     helper_method :qr_code
     before_action :authenticate_user!
-    before_action :authorize_user, only: %i[ validate ]
 
 
     
     def validate
-        @ticket_holder = User.find(@ticket.holder_id)
-        @ticket_seller = User.find(@ticket.seller_id)
-        @ticket_event = Event.find(@ticket.event_id)
-        @is_minor = 18 > ((Time.zone.now - @ticket_holder.date_of_birth.to_time) / 1.year.seconds).floor
+        @ticket_holder = nil
+        @ticket_seller = nil
+        @ticket_event = nil
+        @is_minor = nil
+        if @ticket == nil
+            @validation_message = "This is not a valid ticket."
+        elsif @ticket.seller_id != current_user.id
+            @ticket_holder = User.find(@ticket.holder_id)
+            @validation_message = "Ticket ##{ticket.id}) belonging to #{@ticket_holer.first_name} #{@ticket_holer.last_name}. 
+            You must be logged in as the Host or Ticket Holder to view more information."
+        else
+            @ticket_holder = User.find(@ticket.holder_id)
+            @ticket_seller = User.find(@ticket.seller_id)
+            @ticket_event = Event.find(@ticket.event_id)
+            @is_minor = 18 > ((Time.zone.now - @ticket_holder.date_of_birth.to_time) / 1.year.seconds).floor
+            @validation_method = "Ticket ##{ticket.id}) belonging to #{@ticket_holer.first_name} #{@ticket_holer.last_name}."
+        end
     end
 
     def wallet
@@ -40,13 +52,6 @@ class TicketsController < ApplicationController
             @ticket = nil
         end
     end
-
-    def authorize_user
-        if @ticket.seller_id != current_user.id
-          flash[:alert] = "Ticket validation is for ticket sellers only. You have been re-directed"
-          redirect_to root_path
-        end
-      end
 
     def check_age(dob)
         now = Time.now.utc.to_date
