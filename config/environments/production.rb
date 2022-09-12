78fcdf32913cb9a52b538a8fad7a3a8f61b4ae6b
smith-sj <mr.sjsmith@protonmail.com>
2022-09-12 23:39:58 +1000

    removed :vips again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-  config.active_storage.variant_processor = :vips
 
   
   # Mount Action Cable outside main process or domain.
